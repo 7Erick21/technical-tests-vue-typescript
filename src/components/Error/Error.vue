10a97cf4003e6d9e2ddc0bcd9df4a58dd6063adb
pinedacuevaerick910 <pinedacuevaerick910@gmail.com>
2023-12-12 14:47:44 -0500

    deploy

--- src/components/Error/Error.vue
+++ src/components/Error/Error.vue
@@ -4,8 +4,8 @@ import { ROUTE_LOGIN } from "../../toolbox/constants/allRoutes";
 
 <template>
   <div class="flex flex-col text-center justify-center h-screen gap-10">
-    <div class="font-bold text-9xl text-blue-800">401</div>
-    <div class="text-blue-800 text-xl font-bold">
+    <div class="font-bold text-9xl text-gray-600">401</div>
+    <div class="text-gray-600 text-xl font-bold">
       <router-link :to="ROUTE_LOGIN">Volver al inicio</router-link>
     </div>
   </div>
--- src/pages/NoFound/NoFound.vue
+++ src/pages/NoFound/NoFound.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col text-center justify-center h-screen gap-10">
-    <div class="font-bold text-9xl text-blue-800">404</div>
-    <div class="text-blue-800 text-xl font-bold">
+    <div class="font-bold text-9xl text-gray-600">404</div>
+    <div class="text-gray-600 text-xl font-bold">
       <router-link to="/">Volver al inicio</router-link>
     </div>
   </div>
